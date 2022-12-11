7173488a84ae58b7f56cb8c2faaa3d88c7e91324
sannan <sannan1992@gmail.com>
2022-12-11 23:11:05 +0500

    saving of authentication done

--- lib/UI/post_screen.dart
+++ lib/UI/post_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebasetest/UI/auth/login_screen.dart';
+import 'package:firebasetest/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class postScreen extends StatefulWidget {
   const postScreen({Key? key}) : super(key: key);
@@ -8,10 +11,30 @@ class postScreen extends StatefulWidget {
 }
 
 class _postScreenState extends State<postScreen> {
+  final _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Post Screen"),),
+      appBar: AppBar(
+        title: Text("Post Screen"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _auth.signOut().then((value) {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => loginScreen()));
+              }).onError((error, stackTrace)  {
+                utils().toastMessage(error.toString());
+              });
+            },
+            icon: Icon(Icons.logout_outlined),
+          ),
+          SizedBox(
+            width: 10,
+          )
+        ],
+      ),
     );
   }
 }
--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,12 +1,28 @@
+import 'package:firebasetest/UI/post_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebasetest/UI/auth/login_screen.dart';
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class splashServices{
   void isLogin(BuildContext context){
-    Timer(const Duration(seconds: 3),
-        ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=> loginScreen()))
-    );
+    final _auth = FirebaseAuth.instance;
+
+    final user = _auth.currentUser;
+
+    if(user!= null){
+      Timer(const Duration(seconds: 3),
+              ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=> postScreen()))
+      );
+
+    }
+    else{
+      Timer(const Duration(seconds: 3),
+              ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=> loginScreen()))
+      );
+
+    }
+
 
   }
 }
