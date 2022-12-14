221cb3fcbc0ac72992706ca956715da3961404bd
sannan <sannan1992@gmail.com>
2022-12-14 07:47:38 +0500

    Read operation complete,and also filter the data using search

--- lib/UI/post_screen.dart
+++ lib/UI/post_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebasetest/UI/add_post.dart';
 import 'package:firebasetest/UI/auth/login_screen.dart';
 import 'package:firebasetest/utils/utils.dart';
@@ -13,11 +15,15 @@ class postScreen extends StatefulWidget {
 
 class _postScreenState extends State<postScreen> {
   final _auth = FirebaseAuth.instance;
+  final ref = FirebaseDatabase.instance.ref('test');
+  final searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
         title: Text("Post Screen"),
         actions: [
           IconButton(
@@ -36,9 +42,72 @@ class _postScreenState extends State<postScreen> {
           )
         ],
       ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextFormField(
+              controller: searchController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                hintText: "Search",
+              ),
+              onChanged: (String value) {
+                setState(() {});
+              },
+            ),
+          ),
+          Expanded(
+            child: FirebaseAnimatedList(
+                query: ref,
+                itemBuilder: (context, snapshot, animation, index) {
+                  final title = snapshot.child('title').value.toString();
+                  if (searchController.text.isEmpty) {
+                    return ListTile(
+                      title: Text(snapshot.child('title').value.toString()),
+                      subtitle: Text(snapshot.child('id').value.toString()),
+                      trailing: PopupMenuButton(
+                        icon: Icon(Icons.more_vert),
+                        itemBuilder: (context) => [
+                          PopupMenuItem(
+                            value: 1,
+                            child: ListTile(
+                              title: Text('Edit'),
+                              leading: Icon(Icons.edit),
+                            ),
+                          ),
+                          PopupMenuItem(
+                            value: 1,
+                            child: ListTile(
+                              title: Text('Delete'),
+                              leading: Icon(Icons.delete_outline),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  } else if (title.toLowerCase().contains(
+                      searchController.text.toLowerCase().toString())) {
+                    return ListTile(
+                      title: Text(snapshot.child('title').value.toString()),
+                      subtitle: Text(snapshot.child('id').value.toString()),
+                    );
+                  } else {
+                    return Container();
+                  }
+                }),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> addPostScreen()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => addPostScreen()));
         },
         child: Icon(Icons.add),
       ),
